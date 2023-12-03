f4bd20d51aed9318cf4356520c3a17ed26581fe2
SunsetFelid <skdempsey93@gmail.com>
2023-12-02 17:25:12 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+///Monster raising game
+//needs:
+//Account creation system
+
+//login storage (encryption required!)
+
+//Monster archetypes (the monsters hatch from eggs, and evolve as they grow)
+//each monster has unique stats (strength, defense, HP) that are raised by training
+//each monster has hunger and cleanliness meters that are filled by feeding and bathing respectively,
+// hunger and cleanliness meters being left low for too long is a husbandry failure and affects evolution
+//6 areas to explore and fight other monsters, victories affect evolution, defeats count as a husbandry failure and affect evolution
