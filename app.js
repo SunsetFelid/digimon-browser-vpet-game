a6be6833cbadc65dff2701c9bb597967335850d3
SunsetFelid <skdempsey93@gmail.com>
2023-12-08 21:24:29 -0800

    -started digiEgg function -revamped landing page -blurred background -added GUI location -added buttons for interacting with Digimon -added space for Digimon to exist in

--- app.js
+++ app.js
@@ -1,100 +1,76 @@
 ///Monster raising game
 //needs:
-//variables
-//buttons
-const loginButton = document.getElementsById('login-button');
-const submitButton = document.getElementsById('ac-submit');
-
-//Account Constructor
+//player data
 class user {
     constructor(){
-        this.username = ''; 
-        this.password = '';
-        this.email = '';
-        this.digimon = '';
+        this.egg = {};
+        this.digimon = {};
+        this.graveyard = [];
         }
     }
-//Account creation system
-//Validate input
-if (casPassCreate !== casPassConfirm){
-    alert('Passwords do not match.')
-}
-if (casEmail !== casEmailConfirm){
-    alert('Emails do not match.')
-}
-
-
-function accountCreate(classes){
-    //variables
-    let casUsernameCreate = document.getElementsById('username-create');
-    let casPassCreate = document.getElementsById('password-create');
-    let casPassConfirm = document.getElementsById('password-confirm');
-    let casEmail = document.getElementsById('email');
-    let casEmailConfirm = document.getElementsById('email-confirm');
-    //validate input
-    //make better later!
-    if (casPassCreate.value !== casPassConfirm.value || casEmail.value !== casEmailConfirm){
-        alert('email or password does not match.');
-    } else {
-        username = casUsernameCreate.value;
-        password = casPassCreate.value;
-        email = casEmail.value;
-         new user(username, password, email);
-        
-    }
-};
-//Login form Submissions
-    //login variables
-function validateInput(classes){
-    //variables
-    let username = document.getElementsById('username');
-    let password = document.getElementsById('password');
-    //validate input
-    if(username.value || password.value == ''){
-        alert('Username or password fields are empty.');
-        return;
-    } else {
-        username = "";
-        password = "";
-    }
-}
-function loginForm()
-//event listener for account creation submission
-submitButton = document.addEventListener('ac-submit', (e) => {
-
-})
-//event listener for login form
-loginButton.addEventListener('login-button', (e) => {
-    e.preventDefault();
-    validateInput('username', 'password');
-});
-//login storage (encryption required!)
 
 //Monster archetypes (the monsters hatch from eggs, and evolve as they grow)
 const digitype = ['Virus', 'Vaccine', 'Null'];
 const level = ['Baby','Rookie','Mega']
-const babyNames = ['Botamon', 'Pabumon', 'Poyomon'];
 const rookieNames = ['Agumon', 'Gabumon', 'Patamon', 'Renamon', 'Lopmon', 'Tentamon'];
 const megaNames = ['Wargreymon', 'Weregarurumon', 'Angemon', 'Jijimon', 'Seraphimon', 'Beelzemon', 'MegaKabuterimon'];
+
 //each monster has unique stats (strength, defense, HP) that are raised by training
 class digimon{
     constructor(){
-        let str = [0-9];
-        let def = [0-9];
-        let hp = [20-26];
-        let clean = 100;
-        let hunger = 100;
-        this.name = random(babyNames);
-        this.digitype = random(digitype);
-        this.digistate = level[0];
-        this.str = random(str);
-        this.def = random(def);
-        this.hp = random(hp);
+        this.name = "Botamon";
+        this.digitype = digitype[getRandomInt(3)];
+        this.level = level[0];
+        this.str = getRandomInt(10);
+        this.def = getRandomInt(10);
+        this.hp = 20 + getRandomInt(6);
+        this.age = 0;
         this.clean = 100;
-        this.hunger = 100;
+        this.hunger = 60;
+        this.failure = 0;
+        this.victory = 0;
+    }
+}
+function digiEgg(){
+    const egg = {imgUrl: 'images/Digitama.png', width: 100px}
+    if (user.digimon === '';){
+     
     }
 }
 //each monster has hunger and cleanliness meters that are filled by feeding and bathing respectively,
-// hunger and cleanliness meters being left low for too long is a husbandry failure and affects evolution
+//time (by half hours)
+let seconds = 1799;
+function timer(){
+    seconds--;
+}
+function foodTimer(){
+    if (seconds > 0){
+        setTimeout(timer, 1000);
+    } else {
+        user.digimon.hunger -= 2;
+        seconds = 1799;
+    }
+}
+function messTimer(){
+    if (seconds > 0){
+        setTimeout(tick, 1000);
+    } else { 
+        user.digimon.clean--;
+    }
+}
 
-//6 areas to explore and fight other monsters, victories affect evolution, defeats count as a husbandry failure and affect evolution
+// hunger and cleanliness meters being left low for too long is a husbandry failure and affects evolution
+if (user.digimon.hunger <= 20 || user.digimon.clean <=20){
+    user.digimon.failure += 1;
+}
+function digimonDies(){
+if (user.digimon.hunger || user.digimon.clean === 0){
+    alert('Your Digimon has Died!');
+    let gravestone = user.digimon.name.toString();
+    let lastAge = user.digimon.age.toString() + ' days';
+    user.graveyard.push([gravestone, lastAge]);
+    delete user.digimon;
+    }
+}
+//1 area to explore and fight other monsters, victories affect evolution, defeats count as a husbandry failure and affect evolution
+//battle randomizer (you should be fighting things at your own level)
